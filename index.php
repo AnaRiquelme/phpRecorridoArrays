db35d59a35620286621e6ddf93907cd85a8af3f5
ana <ana@89.29.245.185>
2013-12-24 18:55:20 +0100

    última part. Array asociatiu

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 
 echo "Ejercicio 1. Recorrido de arrays.<br>";
 
-$productos = array ("MESA", "SILLA", "FLEXO", "ESTANTERIA");
+$productos = array("MESA", "SILLA", "FLEXO", "ESTANTERIA");
 
 echo "<br>";
 
@@ -27,10 +27,10 @@ echo "Recorrido del array mediante variables<br>";
 
 echo "<br>";
 
-echo $productos[0] ."<br>";
-echo $productos[1] ."<br>";
-echo $productos[2] ."<br>";
-echo $productos[3] ."<br>";
+echo $productos[0] . "<br>";
+echo $productos[1] . "<br>";
+echo $productos[2] . "<br>";
+echo $productos[3] . "<br>";
 
 echo "<br>";
 
@@ -38,8 +38,8 @@ echo "Recorrido del array escalar (indexada númeriacamente) mediante for sabien
 
 echo "<br>";
 
-for($i=0; $i < 4 ; $i++){
-    echo $productos[$i] ."<br>";
+for ($i = 0; $i < 4; $i++) {
+    echo $productos[$i] . "<br>";
 }
 
 echo "<br>";
@@ -48,8 +48,8 @@ echo "Recorrido del array escalar supuestamente sin conocer el número total de 
 
 echo "<br>";
 
-for($j=0; $j < count($productos) ; $j++){
-    echo $productos[$j] ."<br>";
+for ($j = 0; $j < count($productos); $j++) {
+    echo $productos[$j] . "<br>";
 }
 
 echo "<br>";
@@ -59,9 +59,9 @@ echo "Recorrido del array escalar mediante while<br>";
 echo "<br>";
 
 $m = 0;
-while($m < count($productos)){
-   echo $productos[$m] ."<br>";
-   $m++;
+while ($m < count($productos)) {
+    echo $productos[$m] . "<br>";
+    $m++;
 }
 
 echo "<br>";
@@ -70,8 +70,8 @@ echo "Recorrido del array escalar mediante foreach<br>";
 
 echo "<br>";
 
-foreach ($productos as $valor){
-    echo $valor ."<br>";
+foreach ($productos as $valor) {
+    echo $valor . "<br>";
 }
 
 /*
@@ -89,7 +89,7 @@ echo "<br>";
 
 echo "Ejercicio 2. Recorrido de array unidimensional asociativas.<br>";
 
-$productos2 = array ("M"=>"MESA","S"=>"SILLA","F"=>"FLEXO","E"=>"ESTANTERIA");
+$productos2 = array("M" => "MESA", "S" => "SILLA", "F" => "FLEXO", "E" => "ESTANTERIA");
 
 echo "<br>";
 
@@ -97,10 +97,10 @@ echo "Recorrido del array asociativa mediante variables<br>";
 
 echo "<br>";
 
-echo $productos2["M"] ."<br>";
-echo $productos2["S"] ."<br>";
-echo $productos2["F"] ."<br>";
-echo $productos2["E"] ."<br>";
+echo $productos2["M"] . "<br>";
+echo $productos2["S"] . "<br>";
+echo $productos2["F"] . "<br>";
+echo $productos2["E"] . "<br>";
 
 echo "<br>";
 
@@ -109,9 +109,8 @@ echo "Recorrido del array escalar mediante while<br>";
 echo "<br>";
 
 
-while(list(,$value) = each($productos2)){
-   echo $value ."<br>";
-  
+while (list(, $value) = each($productos2)) {
+    echo $value . "<br>";
 }
 
 echo "<br>";
@@ -120,8 +119,8 @@ echo "Recorrido del array escalar mediante foreach<br>";
 
 echo "<br>";
 
-foreach ($productos2 as $valor){
-    echo $valor ."<br>";
+foreach ($productos2 as $valor) {
+    echo $valor . "<br>";
 }
 
 /*
@@ -129,7 +128,7 @@ foreach ($productos2 as $valor){
  *  
  * i) Recorrido del array bidimensional mediante el uso de variables.
  * 
- *j) Recorrido del array bidimensional usando un bucle while y un bucle for.
+ * j) Recorrido del array bidimensional usando un bucle while y un bucle for.
  *
  * k) Recorrido del array bidimensional mediante el uso de dos for.
  * 
@@ -141,11 +140,11 @@ echo "<br>";
 
 echo "Ejercicio 3. Array bidimensional escalar.<br>";
 
-$animales = array(array("Perro","Mastin", 100 ),//00-01-02
-array("Gato","Persa", 150 ),//10-11-12
-array("Gato","Siames", 190 ),//20-21-22
-array("Perro","Boxer", 25 ), //30-31-32
-array("Canario","Timbrado", 250 ));//40-41-42
+$animales = array(array("Perro", "Mastin", 100), //00-01-02
+    array("Gato", "Persa", 150), //10-11-12
+    array("Gato", "Siames", 190), //20-21-22
+    array("Perro", "Boxer", 25), //30-31-32
+    array("Canario", "Timbrado", 250)); //40-41-42
 
 print_r($animales);
 
@@ -158,25 +157,25 @@ echo "Recorrido del array mediante variables<br>";
 
 echo "<br>";
 
-echo $animales[0][0] ." ";
-echo $animales[0][1] ." ";
-echo $animales[0][2] ."<br>";
+echo $animales[0][0] . " ";
+echo $animales[0][1] . " ";
+echo $animales[0][2] . "<br>";
 
-echo $animales[1][0] ." ";
-echo $animales[1][1] ." ";
-echo $animales[1][2] ."<br>";
+echo $animales[1][0] . " ";
+echo $animales[1][1] . " ";
+echo $animales[1][2] . "<br>";
 
-echo $animales[2][0] ." ";
-echo $animales[2][1] ." ";
-echo $animales[2][2] ."<br>";
+echo $animales[2][0] . " ";
+echo $animales[2][1] . " ";
+echo $animales[2][2] . "<br>";
 
-echo $animales[3][0] ." ";
-echo $animales[3][1] ." ";
-echo $animales[3][2] ."<br>";
+echo $animales[3][0] . " ";
+echo $animales[3][1] . " ";
+echo $animales[3][2] . "<br>";
 
-echo $animales[4][0] ." ";
-echo $animales[4][1] ." ";
-echo $animales[4][2] ."<br>";
+echo $animales[4][0] . " ";
+echo $animales[4][1] . " ";
+echo $animales[4][2] . "<br>";
 
 echo "<br>";
 
@@ -184,15 +183,15 @@ echo "Recorrido del array mediante while y for<br>";
 
 echo "<br>";
 
-$c=0;
+$c = 0;
 
-for($d = 0; $d < count($animales); $d++){
-    while($c < count($animales)){
-         echo $animales[$d][$c] . " ";
-         $c++;
+for ($d = 0; $d < count($animales); $d++) {
+    while ($c < count($animales)) {
+        echo $animales[$d][$c] . " ";
+        $c++;
     }
     echo "<br>";
-    $c=0;
+    $c = 0;
 }
 
 echo "<br>";
@@ -201,8 +200,8 @@ echo "Recorrido del array mediante dos for<br>";
 
 echo "<br>";
 
-for($e = 0; $e < count($animales); $e++){
-    for($f = 0; $f < count($animales); $f++){
+for ($e = 0; $e < count($animales); $e++) {
+    for ($f = 0; $f < count($animales); $f++) {
         echo $animales[$e][$f] . "<br>";
     }
 }
@@ -213,8 +212,28 @@ echo "Recorrido del array mediante dos foreach<br>";
 
 echo "<br>";
 
-//foreach (){
-//    
-//}
+foreach ($animales as $key => $value) {
+    foreach ($value as $valor) {
+       echo $valor . " ";
+    }
+      echo "<br>";
+}
+
+/*
+ * Realizar:
+ *  
+ * i) Recorrido del array bidimensional mediante el uso de variables.
+ * 
+ * j) Recorrido del array bidimensional usando un bucle while y un bucle for.
+ *
+ * k) Recorrido del array bidimensional mediante el uso de dos for.
+ * 
+ * l) Recorrido del array bidimensional mediante el uso de dos foreach.
+ * 
+ */
+
+echo "<br>";
+
+echo "Ejercicio 3. Array bidimensional escalar.<br>";
 
 ?>
